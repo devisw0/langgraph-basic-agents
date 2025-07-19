3d0ac9146342192260e77a3cccb5f8993fac6c4b
Devan P. <91017674+devisw0@users.noreply.github.com>
2025-07-19 02:33:56 -0400

    Changed Folder name
    Changed Folder Name

---
+++ Graph 2/graph 2.py
@@ -0,0 +1,8 @@
+from typing import TypedDict, Dict
+from langgraph import StateGraph
+
+#main goal of this graph is how to handle multiple inputs, perform operations on list data, and have a more complex AgentState
+
+
+
+
